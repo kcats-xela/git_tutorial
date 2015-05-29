1b04e64f75300c33e7638ec2cfef9d25703bc643
Alex Chung <ydochung@rescomp-13-267546.stanford.edu>
2015-05-29 01:04:12 -0700

    modified ProductFileReader.java to calculate average score

--- kx.loader.product/src/main/java/com/ydochung/kx/loader/product/ProductFileReader.java
+++ kx.loader.product/src/main/java/com/ydochung/kx/loader/product/ProductFileReader.java
@@ -1,29 +1,37 @@
 package com.ydochung.kx.loader.product;
 
-import java.io.BufferedReader;
 import java.io.*;
+import java.util.ArrayList;
 
 public class ProductFileReader {
-
 	public static void main(String[] args) {
 		String inputFile = "inputFile/products.txt";
+		ArrayList <Double> grades = new ArrayList <Double>();
+		readFileAndGetAverage(inputFile, grades);
+		
+	}
+
+	public static void readFileAndGetAverage(String filename, ArrayList<Double> grades){
+		String inputFile = filename;
+		double sum = 0;
 		try{			
-			BufferedReader br = 
-					new BufferedReader(new FileReader(inputFile));
-			
+			BufferedReader br = new BufferedReader(new FileReader(inputFile));
 			while (true){
-				String line = br.readLine();
-				if (line == null) break;
-				System.out.println(line);
+				String name  = br.readLine();
+				String grade = br.readLine();
+				if (name == null) break;
+				double d_grade = Double.parseDouble(grade);
+				grades.add(d_grade);
 			}
+			int numStudents = grades.size();
+			for (int i = 0; i < numStudents; i++){
+				sum += grades.get(i);
+			}
+			System.out.println("Average of all students: " + sum/numStudents);
 			br.close();
 		}
 		catch (IOException ex){
 			System.out.println("Can't open that file");
-			
-		}
-
-			
+		}	
 	}
-
 }
