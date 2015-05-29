c93622c61a87c401ed6414efa6a4117fa10a794c
Seungtack Baek <Seungtack_Baek@neimanmarcus.com>
2015-05-28 22:13:15 -0500

    initial * created new project java.tutorial for alex * created simple maven structure (src/main/java, src/test/java)

---
+++ java.tutorial/src/main/java/com/keabkcats/java/tutorial/App.java
@@ -0,0 +1,13 @@
+package com.keabkcats.java.tutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
